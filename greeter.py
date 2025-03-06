d681b23637c3d80459ba942a368bf5b66cb5e7ec
BryanTrieu <bamboob0y@hotmail.com>
2025-03-06 15:45:23 -0800

    Add files via upload

--- greeter.py
+++ greeter.py
@@ -1,4 +1,31 @@
+# introduction to inputs
 prompt = "If you tell us who you are, we can personalize the messages you see."
 prompt += "\nWhat is your first name? "
 name = input(prompt)
 print(f"\nHello, {name}!")
+
+# defining a function
+def greet_user():
+	"""Display a simple greeting."""
+	print("Hello!")
+
+greet_user()
+
+# passing information to function
+def greet_user(username):
+	"""Display a simple greeting."""
+	print(f"Hello, {username.title()}!")
+greet_user('jesse')
+
+# function to display message
+def display_message():
+	"""Displays message regarding learning about functions"""
+	print("I learned about functions in chapter 8!")
+
+display_message()
+
+# 8-2 Favorite Book function example
+def favorite_book(title):
+	print(f"One of my favorite books is {title.title()}.")
+title = 'Alice in Wonderland'
+favorite_book(title)
